22592e9eaa1f7d8d9fd09a7d4fd00725ba849aaf
intel0620 <intel0620@gmail.com>
2023-04-28 11:38:37 +0800

    加入專案檔案。

---
+++ UnifiedAuthDemo/Models/TwitterLoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UnifiedAuthDemo.Models
+{
+    public class TwitterLoginModel
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectUri { get; set; }
+        public string Scope { get; set; }
+    }
+}
